ad0eb0fc5981f88444dbe8bf3b7dd0faf4f86414
jilldenise <jildenise2183@gmail.com>
2022-11-21 15:09:16 +0000

    Update StatusEffectTracker.cs

--- LostArkLogger/Data/StatusEffectTracker.cs
+++ LostArkLogger/Data/StatusEffectTracker.cs
@@ -67,7 +67,7 @@ namespace LostArkLogger
             {
                 Entity sourceEntity = parser.GetSourceEntity(sourceId);
                 if (effectData.Value == null) return;
-                var amount = (effectData.hasValue > 0 && effectData.Value.Length == 4) ? BitConverter.ToInt32(effectData.Value, 0) : 0;
+                var amount = (effectData.hasValue > 0 && effectData.Value != null && effectData.Value.Length == 4) ? BitConverter.ToInt32(effectData.Value, 0) : 0;
                 var statusEffect = new StatusEffect { Started = DateTime.UtcNow, StatusEffectId = effectData.StatusEffectId, InstanceId = effectData.EffectInstanceId, SourceId = sourceEntity.EntityId, TargetId = targetId, Type = effectType, Value = amount };
                 // end this buf now, it got refreshed
                 if (effectList.TryRemove(statusEffect.InstanceId, out var oldStatusEffect))
