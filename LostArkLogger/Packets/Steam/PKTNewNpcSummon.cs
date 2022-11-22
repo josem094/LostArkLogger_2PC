9bf45281162f2e2e7aa0fe73f0139f084986f915
robot <josem094@hotmail.com>
2022-11-22 11:28:09 -0500

    additions

--- LostArkLogger/Packets/Steam/PKTNewNpcSummon.cs
+++ LostArkLogger/Packets/Steam/PKTNewNpcSummon.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 namespace LostArkLogger
@@ -6,9 +7,9 @@ namespace LostArkLogger
     {
         public void SteamDecode(BitReader reader)
         {
-            b_0 = reader.ReadByte();
             bytearray_0 = reader.ReadBytes(18);
             OwnerId = reader.ReadUInt64();
+            b_0 = reader.ReadByte();
             bytearray_1 = reader.ReadBytes(13);
             npcStruct = reader.Read<NpcStruct>();
         }
